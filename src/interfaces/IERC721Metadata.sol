5aeab9b78aac2217981eb2a47849568d849deee6
crypt0 <crypt0@m374-crypt0>
2025-04-29 15:16:52 +0000

    chore: populated interfaces

--- src/interfaces/IERC721Metadata.sol
+++ src/interfaces/IERC721Metadata.sol
@@ -1,24 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/* ERC721 Metadata JSON schema
-  {
-    "title": "Asset Metadata",
-    "type": "object",
-    "properties": {
-      "name": {
-        "type": "string",
-        "description": "Identifies the asset to which this NFT represents"
-      },
-      "description": {
-        "type": "string",
-        "description": "Describes the asset to which this NFT represents"
-      },
-      "image": {
-        "type": "string",
-        "description": "A URI pointing to a resource with mime type image/* representing the asset to which this NFT represents. Consider making any images at a width between 320 and 1080 pixels and aspect ratio between 1.91:1 and 4:5 inclusive."
-      }
-    }
-  }
- */
-interface IERC721Metadata { }
+import { IERC721 } from "./IERC721.sol";
+
+/// @title ERC-721 Non-Fungible Token Standard, optional metadata extension
+/// @dev See https://eips.ethereum.org/EIPS/eip-721
+///  Note: the ERC-165 identifier for this interface is 0x5b5e139f.
+interface ERC721Metadata is IERC721 {
+  /// @notice A descriptive name for a collection of NFTs in this contract
+  function name() external view returns (string calldata name);
+
+  /// @notice An abbreviated name for NFTs in this contract
+  function symbol() external view returns (string calldata symbol);
+
+  /// @notice A distinct Uniform Resource Identifier (URI) for a given asset.
+  /// @dev Throws if `tokenId` is not a valid NFT. URIs are defined in RFC
+  ///  3986. The URI may point to a JSON file that conforms to the "ERC721
+  ///  Metadata JSON Schema".
+  /// @param tokenId The identifier of the NFT to query
+  function tokenURI(uint256 tokenId) external view returns (string calldata uri);
+}
