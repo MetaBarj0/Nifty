9ddd4d9850e9e2eb874b6388a91825752b1b7998
crypt0 <crypt0@m374-crypt0>
2025-04-29 14:05:11 +0000

    chore: test and interfaces scaffolding

---
+++ test/Nifty.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
+
+import { Test } from "forge-std/Test.sol";
+
+contract DeploymentTests is Test {
+  function setUp() public { }
+
+  function test_deploy_creatorIsSet() public pure {
+    assertEq(true, false);
+  }
+}
