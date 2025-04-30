17fbda777f785012391b35b0ce230eaa466e7aa2
crypt0 <crypt0@m374-crypt0>
2025-04-30 14:53:49 +0000

    test: ERC165

--- src/Nifty.sol
+++ src/Nifty.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.29;
 
-contract Nifty {
+import { ERC165 } from "./ERC165.sol";
+import { INifty } from "./interfaces/INifty.sol";
+
+contract Nifty is INifty, ERC165 {
   address public immutable creator;
 
   constructor() {
     creator = msg.sender;
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+    return interfaceId == type(INifty).interfaceId || super.supportsInterface(interfaceId);
   }
 }
---
+++ test/ERC165.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.29;
+
+import { Nifty } from "../src/Nifty.sol";
+import { IERC165 } from "../src/interfaces/IERC165.sol";
+import { INifty } from "../src/interfaces/INifty.sol";
+
+import { Test } from "forge-std/Test.sol";
+
+contract DeploymentTests is Test {
+  Nifty private token;
+
+  function setUp() public {
+    token = new Nifty();
+  }
+
+  function test_supports_interface() public view {
+    assertEq(token.supportsInterface(type(IERC165).interfaceId), true);
+    assertEq(token.supportsInterface(type(INifty).interfaceId), true);
+  }
+}
