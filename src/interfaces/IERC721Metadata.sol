d1e34c8e83f9dbcbe68afb375bc65bfd402f11cb
crypt0 <crypt0@m374-crypt0.xyz>
2025-05-18 10:21:59 +0000

    docs: IOwnable2Steps
    documenting IOwnable2Steps
    interface seggregation

--- src/interfaces/IERC721Metadata.sol
+++ src/interfaces/IERC721Metadata.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IERC721 } from "./IERC721.sol";
-
 /// @title ERC-721 Non-Fungible Token Standard, optional metadata extension
 /// @dev See https://eips.ethereum.org/EIPS/eip-721
 ///  Note: the ERC-165 identifier for this interface is 0x5b5e139f.
-interface IERC721Metadata is IERC721 {
+interface IERC721Metadata {
   /// @notice A descriptive name for a collection of NFTs in this contract
   function name() external view returns (string memory);
 
--- src/interfaces/IOwnable2Steps.sol
+++ src/interfaces/IOwnable2Steps.sol
@@ -1,11 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// TODO: document
 interface IOwnable2Steps {
+  /// @notice Get the current owner of the contract implementation
+  /// @dev Single owner, may be changed in 2 steps.
   function owner() external view returns (address);
+
+  /// @notice Get the pending owner. The pending owner must accept the
+  ///  ownership change by calling acceptOwnership
+  /// @dev Single owner, may be changed in 2 steps.
   function pendingOwner() external view returns (address);
+
+  /// @notice start the process of changing the actual owner or reset the
+  ///  process of changing the actual owner
+  /// @dev Throws if sender is not the actual owner. The new owner has to
+  ///  accept the ownership before owner change to be effective.
+  /// @param newOwner the new owner the actual owner wants to transfer the
+  ///  ownership to. If set with address(0), no change in ownership is asked
+  ///  anymore.
   function transferOwnership(address newOwner) external;
+
+  /// @notice finish the process of changing the actual owner.
+  /// @dev Throws unless sender is the pending owner specified by a
+  ///  transferOwnership call.
   function acceptOwnership() external;
+
+  /// @notice renounce to the ownership.
+  /// @dev After this call, no one owns the contract anymore and nobody can own
+  ///  this contract anymore in the future.
   function renounceOwnership() external;
 }
