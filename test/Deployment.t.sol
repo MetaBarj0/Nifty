6aa65bbd2ba2da4f7545003a0fdb534b1778e8cf
crypt0 <crypt0@m374-crypt0.xyz>
2025-09-23 14:11:22 +0000

    test: deployment
    multiple sut
    - implementation
    - transparent proxy
    need to find a way to cycle sut to test both the implementation and the
    proxy calling the underlying implementation

--- test/Deployment.t.sol
+++ test/Deployment.t.sol
@@ -5,14 +5,28 @@ import { Nifty } from "../src/token/Nifty.sol";
 
 import { Test } from "forge-std/Test.sol";
 
+import { TransparentUpgradeableProxy } from "../src/proxy/TransparentUpgradeableProxy.sol";
 import { NiftyTestUtils } from "./NiftyTestUtils.sol";
 
 contract DeploymentTests is Test, NiftyTestUtils {
+  address alice;
+
   function setUp() public {
     nifty = new Nifty();
+    proxy = new TransparentUpgradeableProxy(address(nifty), "");
+
+    alice = makeAddr("alice");
+    vm.startPrank(alice);
   }
 
-  function test_deploy_creatorIsSet() public view {
-    assertEq(nifty.creator(), address(this));
+  function test_deploy_creatorIsSet() public {
+    (bool implSuccess, bytes memory implData) = address(nifty).call(abi.encodeWithSignature("creator()"));
+    (bool proxySuccess, bytes memory proxyData) = address(proxy).call(abi.encodeWithSignature("creator()"));
+
+    assertTrue(implSuccess);
+    assertEq(address(this), abi.decode(implData, (address)));
+
+    assertTrue(proxySuccess);
+    assertEq(address(this), abi.decode(proxyData, (address)));
   }
 }
--- test/NiftyTestUtils.sol
+++ test/NiftyTestUtils.sol
@@ -3,10 +3,12 @@ pragma solidity 0.8.30;
 
 import { Test } from "forge-std/Test.sol";
 
+import { TransparentUpgradeableProxy } from "../src/proxy/TransparentUpgradeableProxy.sol";
 import { Nifty } from "../src/token/Nifty.sol";
 
 abstract contract NiftyTestUtils is Test {
   Nifty internal nifty;
+  TransparentUpgradeableProxy internal proxy;
 
   function paidMint(address to, uint256 tokenId) internal {
     vm.deal(to, 500 gwei);
