fee4b4e045534f5b3459244f7dd380c0e0428f6f
crypt0 <crypt0@m374-crypt0.xyz>
2025-11-04 10:06:21 +0000

    fix: contracts - tests
    fixed a stack too deep error in permit test cases

--- contracts/test/token/ERC721Permit.t.sol
+++ contracts/test/token/ERC721Permit.t.sol
@@ -23,8 +23,9 @@ contract ERC721Tests is NiftyTestUtils {
   }
 
   function table_permit_throws_withExpiredDeadline(SUTDatum memory sutDatum) public {
-    (address owner, address spender, uint256 tokenId, uint256 deadline, uint256 nonce, uint8 v, bytes32 r, bytes32 s) =
-      getPermitData_(alice, bob, 0, block.timestamp + 10 minutes, 0);
+    uint256 deadline = block.timestamp + 10 minutes;
+
+    (uint8 v, bytes32 r, bytes32 s) = getPermitSignature_(aliceKey, alice, bob, 0, deadline, 0);
 
     skip(20 minutes);
 
@@ -33,44 +34,30 @@ contract ERC721Tests is NiftyTestUtils {
       sutDatum.sut,
       alice,
       abi.encodeWithSignature(
-        "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)",
-        owner,
-        spender,
-        tokenId,
-        deadline,
-        nonce,
-        v,
-        r,
-        s
+        "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)", alice, bob, 0, deadline, 0, v, r, s
       )
     );
   }
 
   function table_permit_throws_withInvalidNonce(SUTDatum memory sutDatum) public {
-    (address owner, address spender, uint256 tokenId, uint256 deadline, uint256 nonce, uint8 v, bytes32 r, bytes32 s) =
-      getPermitData_(alice, bob, 0, block.timestamp + 10 minutes, 123);
+    uint256 deadline = block.timestamp + 10 minutes;
+
+    (uint8 v, bytes32 r, bytes32 s) = getPermitSignature_(aliceKey, alice, bob, 0, deadline, 123);
 
     expectCallRevert(
       IERC721Permit.InvalidNonce.selector,
       sutDatum.sut,
       alice,
       abi.encodeWithSignature(
-        "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)",
-        owner,
-        spender,
-        tokenId,
-        deadline,
-        nonce,
-        v,
-        r,
-        s
+        "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)", alice, bob, 0, deadline, 123, v, r, s
       )
     );
   }
 
   function table_permit_throws_withIncorrectRecoveredAddress(SUTDatum memory sutDatum) public {
-    (address owner, address spender, uint256 tokenId, uint256 deadline, uint256 nonce, uint8 v, bytes32 r, bytes32 s) =
-      getPermitData_(alice, bob, 0, block.timestamp + 10 minutes, 0);
+    uint256 deadline = block.timestamp + 10 minutes;
+
+    (uint8 v, bytes32 r, bytes32 s) = getPermitSignature_(aliceKey, alice, bob, 0, deadline, 0);
 
     expectCallRevert(
       IERC721Permit.InvalidSigner.selector,
@@ -78,11 +65,11 @@ contract ERC721Tests is NiftyTestUtils {
       alice,
       abi.encodeWithSignature(
         "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)",
-        spender, // swapped owner and spender
-        owner,
-        tokenId,
+        bob, // swapped owner and spender
+        alice,
+        0,
         deadline,
-        nonce,
+        0,
         v,
         r,
         s
@@ -91,23 +78,16 @@ contract ERC721Tests is NiftyTestUtils {
   }
 
   function table_throws_forNotOwnedToken(SUTDatum memory sutDatum) public {
-    (address owner, address spender, uint256 tokenId, uint256 deadline, uint256 nonce, uint8 v, bytes32 r, bytes32 s) =
-      getPermitData_(alice, bob, 0, block.timestamp + 10 minutes, 0);
+    uint256 deadline = block.timestamp + 10 minutes;
+
+    (uint8 v, bytes32 r, bytes32 s) = getPermitSignature_(aliceKey, alice, bob, 0, deadline, 0);
 
     expectCallRevert(
       INifty.Unauthorized.selector,
       sutDatum.sut,
       bob,
       abi.encodeWithSignature(
-        "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)",
-        owner,
-        spender,
-        tokenId,
-        deadline,
-        nonce,
-        v,
-        r,
-        s
+        "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)", alice, bob, 0, deadline, 0, v, r, s
       )
     );
   }
@@ -120,45 +100,35 @@ contract ERC721Tests is NiftyTestUtils {
 
     assertEq(0, callForUint256(sut, alice, abi.encodeWithSignature("nonces(address)", alice)));
 
-    (address owner, address spender, uint256 tokenId, uint256 deadline, uint256 nonce, uint8 v, bytes32 r, bytes32 s) =
-      getPermitData_(alice, bob, 0, block.timestamp + 10 minutes, 0);
+    uint256 deadline = block.timestamp + 10 minutes;
+
+    (uint8 v, bytes32 r, bytes32 s) = getPermitSignature_(aliceKey, alice, bob, 0, deadline, 0);
 
     vm.expectEmit();
-    emit IERC721.Approval(owner, spender, tokenId);
+    emit IERC721.Approval(alice, bob, 0);
 
     callForVoid(
       sut,
       bob,
       abi.encodeWithSignature(
-        "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)",
-        owner,
-        spender,
-        tokenId,
-        deadline,
-        nonce,
-        v,
-        r,
-        s
+        "permit(address,address,uint256,uint256,uint64,uint8,bytes32,bytes32)", alice, bob, 0, deadline, 0, v, r, s
       )
     );
 
-    assertEq(spender, callForAddress(sut, spender, abi.encodeWithSignature("getApproved(uint256)", 0)));
-    assertEq(1, callForUint256(sut, owner, abi.encodeWithSignature("nonces(address)", owner)));
+    assertEq(bob, callForAddress(sut, bob, abi.encodeWithSignature("getApproved(uint256)", 0)));
+    assertEq(1, callForUint256(sut, alice, abi.encodeWithSignature("nonces(address)", alice)));
   }
 
-  function getPermitData_(address owner, address spender, uint256 tokenId, uint256 deadline, uint256 nonce)
-    private
-    view
-    returns (address ow, address sp, uint256 t, uint256 d, uint256 n, uint8 v, bytes32 r, bytes32 s)
-  {
+  function getPermitSignature_(
+    uint256 ownerKey,
+    address owner,
+    address spender,
+    uint256 tokenId,
+    uint256 deadline,
+    uint256 nonce
+  ) private pure returns (uint8 v, bytes32 r, bytes32 s) {
     bytes32 h = keccak256(abi.encode(owner, spender, tokenId, deadline, nonce));
 
-    ow = owner;
-    sp = spender;
-    t = tokenId;
-    d = deadline;
-    n = nonce;
-
-    (v, r, s) = vm.sign(aliceKey, h);
+    (v, r, s) = vm.sign(ownerKey, h);
   }
 }
